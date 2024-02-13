527007b4aa19c3f24f6d239c4ca62bd26ec50b34
바악경일 <129853254+pki071120@users.noreply.github.com>
2024-02-13 16:01:31 +0900

    Delete dist directory

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-// let a:number = 3
-// let b:string = "hi typescript"
-// let c:any = 4
-// c = "string"
-function addNum(a, b) {
-    return a + b;
-}
-addNum(3, 7);
-//# sourceMappingURL=index.js.map
